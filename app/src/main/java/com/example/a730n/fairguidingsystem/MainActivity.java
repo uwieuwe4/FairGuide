19f024e7110d0a228fd971f73d5c5ec1ede1d74d
uwieuwe4 <noel.dinger@gmail.com>
2018-04-22 22:45:13 +0200

    new

--- app/src/main/java/com/example/a730n/fairguidingsystem/MainActivity.java
+++ app/src/main/java/com/example/a730n/fairguidingsystem/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
 
+    //NEW
     BluetoothAdapter mBluetoothAdapter;
     // Button btnEnableDisable_Discoverable;
     Button btnTxOK;
